7a1f1e75e46bda2c9a3a1ac1e04ffe8f53d39d33
KristalD <krisdivitku@gmail.com>
2019-02-18 08:58:29 +0100

    Initial commit

---
+++ app/src/main/java/com/example/walkabuddy/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.walkabuddy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tv;
+    private ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.tv);
+        iv = (ImageView) findViewById(R.id.iv);
+        Animation myanim = AnimationUtils.loadAnimation(MainActivity.this,R.anim.mytransition);
+        tv.startAnimation(myanim);
+        iv.startAnimation(myanim);
+
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally{
+                    startActivity(new Intent(MainActivity.this, MapsActivity.class));
+                    finish();
+                }
+
+
+            }
+        };
+        timer.start();
+    }
+}
